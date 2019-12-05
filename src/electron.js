f0b9375b163b4a447f933a591161e683b3a4c92a
Gillmann <adrien.gillmann@gmail.com>
2019-12-05 13:45:30 +0100

    devtools react

--- src/electron.js
+++ src/electron.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
+const os = require('os');
 
 const { app } = electron;
 const { BrowserWindow } = electron;
@@ -13,6 +14,37 @@ function createWindow() {
     height: 680,
   });
 
+  // DEVTOOLS REACT
+  if (isDev) {
+    switch (os.platform()) {
+      case 'linux':
+        BrowserWindow.addDevToolsExtension(
+          path.join(
+            os.homedir(),
+            '/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.2.1_0',
+          ),
+        );
+        break;
+      case 'windows':
+        BrowserWindow.addDevToolsExtension(
+          path.join(
+            os.homedir(),
+            '/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.2.1_0',
+          ),
+        );
+        break;
+      case 'mac':
+        BrowserWindow.addDevToolsExtension(
+          path.join(
+            os.homedir(),
+            '/Library/Application Support/Google/Chrome/Default/Extensions/' +
+              'fmkadmapgofadopljbjfkapdkoienihi/4.2.1_0',
+          ),
+        );
+        break;
+      default:
+    }
+  }
   mainWindow.loadURL(
     isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`,
   );
