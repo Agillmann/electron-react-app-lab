8ab4a504f55780142e33c98f81a06b2da6ff4f74
Gillmann <adrien.gillmann@gmail.com>
2019-12-05 14:25:43 +0100

    Material navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-const Navbar = () => (
-  <header className="">
-    <nav>
-      <ul className="">
-        <li className="">
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-        </li>
-        <li className="">
-          <NavLink to="/test" exact>
-            Test
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const useStyles = makeStyles({
+  root: {
+    width: 'auto',
+  },
+});
 
-export default Navbar;
+export default function Navbar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+      <BottomNavigationAction
+        component={Link}
+        to="/"
+        label="Home"
+        value="home"
+        icon={<HomeIcon />}
+      />
+      <BottomNavigationAction
+        component={Link}
+        to="/map"
+        label="Map"
+        value="map"
+        icon={<LocationOnIcon />}
+      />
+    </BottomNavigation>
+  );
+}
