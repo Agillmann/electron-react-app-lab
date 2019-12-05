7f88d67f6d6075fc0ec99615f520d16b188888a5
Gillmann <adrien.gillmann@gmail.com>
2019-12-05 10:08:17 +0100

    add transition between route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { withRouter, Switch, Route } from 'react-router-dom'
+
 import Layout from './components/Layout'
 import HomePage from './components/HomePage'
 import NotFoundPage from './components/NotFoundPage'
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
 const App = ({ location }) => {  
   return (
   	<Layout>
-      <Switch location={location}>
-        <Route path="/" exact component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <TransitionGroup className="transition-group">
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={'fade'}
+        >
+        <section className="route-section">
+          <Switch location={location}>
+            <Route path="/" exact component={HomePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </section>
+        </CSSTransition>
+      </TransitionGroup>
     </Layout>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom'
 import App from "./App";
 import CssBaseline from '@material-ui/core/CssBaseline';
-
+import './assets/styles/styles.scss'
 ReactDOM.render((
     <BrowserRouter>
       <CssBaseline>
